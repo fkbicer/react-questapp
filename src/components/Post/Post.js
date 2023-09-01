95e45b24e9de8dd7a45b70c5ef188b7e88510921
furkankaan <fkbicer07@gmail.com>
2023-09-01 18:33:14 +0300

    post, home components updated

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,43 +1,15 @@
-
 import React, {useState, useEffect} from "react";
 import ReactDOM from "react-dom";
+import './Post.scss';
 
-function Post() {
-    const [error, setError] = useState(null);
-    const [isLoaded, setIsLoaded] = useState(false);
-    const [postList, setPostList] = useState([]);
-
-    useEffect(
-        () => {
-            fetch("/posts")
-            .then(res => res.json())
-            .then(
-                (result) => {
-                    setIsLoaded(true);
-                    setPostList(result);
-                },
-                (error) => {
-                    setIsLoaded(true);
-                    setError(error);
-                }
-            )
-        }, [] )
-        if(error) {
-            return <div> Error !! </div>
-        }else if (!isLoaded) {
-            <div> Loading ...</div>
-        }else {
-            return (
-                <ul>
-                    {postList.map(post => (
-                        <li>
-                            {post.title} {post.text}
-                        </li>
-                    ))}
-                </ul>
-            )
-        }
-
+function Post(props) {
+    const{title,text} = props;
+    return(
+        <div className="postContainer">
+            {title}
+            {text}
+        </div>
+    )
 }
 
 export default Post;
