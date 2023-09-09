9da87533f1d6c676b66186081a2e3b174f217b10
furkankaan <fkbicer07@gmail.com>
2023-09-09 16:56:20 +0300

    added savepost to postform

--- src/components/Post/PostForm.js
+++ src/components/Post/PostForm.js
@@ -14,7 +14,8 @@ import FavoriteIcon from '@mui/icons-material/Favorite';
 import { makeStyles } from '@mui/styles';
 import CommentIcon from '@mui/icons-material/Comment';
 import {Link} from "react-router-dom"
-import { OutlinedInput } from "@mui/material";
+import { InputAdornment, OutlinedInput } from "@mui/material";
+import { Button } from "@mui/material";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -41,16 +42,41 @@ const ExpandMore = styled((props) => {
   }));
 
 function PostForm(props) { 
-    const {text,title,userName,userId} = props;
+    const {userName,userId} = props;
     const classes = useStyles();
-    const [expanded, setExpanded] = useState(false);
-    const [liked, setLiked] = useState(false);
-    const handleExpandClick = () => {
-        setExpanded(!expanded);
-      }
-    const handleLike = () => {
-      setLiked(!liked);
+    const [text, SetText] = useState("");
+    const [title, SetTitle] = useState("");
+
+    const savePost = () => {
+        fetch("/posts",
+        {
+            method: "POST",
+            headers: {"Content-Type" : "application/json",},
+            body : JSON.stringify({
+                title:title,
+                userId: userId,
+                text : text,
+            }),
+        })
+        .then((res) => res.json())
+        .catch((err) => console.log("error"))
     }
+
+    const handleSubmit = () => {
+        savePost();
+    }
+
+    const handleTitle = (value) => {
+        SetTitle(value);
+    }
+
+    
+    const handleText = (value) => {
+        SetText(value);
+    }
+
+    
+ 
 
 
 
@@ -71,6 +97,7 @@ function PostForm(props) {
           placeholder= "Title"
           inputProps={{maxLength :25}}
           fullWidth
+          onChange={ (i) => handleTitle(i.target.value)}
           >
 
           </OutlinedInput>}
@@ -83,53 +110,21 @@ function PostForm(props) {
           placeholder= "Text"
           inputProps={{maxLength :250}}
           fullWidth
+          onChange={ (i) => handleText(i.target.value)}
+          endAdornment = {
+            <InputAdornment position = "end">
+                <Button
+                variant = "contained"
+                onClick= {handleSubmit}
+                
+                > Post </Button>
+            </InputAdornment>
+          }
           >
 
           </OutlinedInput>}
           </Typography>
         </CardContent>
-        <CardActions disableSpacing>
-          <IconButton onClick={handleLike} aria-label="add to favorites">
-            <FavoriteIcon style={liked? {color :'red'} : null} />
-          </IconButton>
-          <ExpandMore
-            expand={expanded}
-            onClick={handleExpandClick}
-            aria-expanded={expanded}
-            aria-label="show more"
-          >
-            <CommentIcon />
-          </ExpandMore>
-        </CardActions>
-        <Collapse in={expanded} timeout="auto" unmountOnExit>
-          <CardContent>
-            <Typography paragraph>Method:</Typography>
-            <Typography paragraph>
-              Heat 1/2 cup of the broth in a pot until simmering, add saffron and set
-              aside for 10 minutes.
-            </Typography>
-            <Typography paragraph>
-              Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over
-              medium-high heat. Add chicken, shrimp and chorizo, and cook, stirring
-              occasionally until lightly browned, 6 to 8 minutes. Transfer shrimp to a
-              large plate and set aside, leaving chicken and chorizo in the pan. Add
-              pimentón, bay leaves, garlic, tomatoes, onion, salt and pepper, and cook,
-              stirring often until thickened and fragrant, about 10 minutes. Add
-              saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
-            </Typography>
-            <Typography paragraph>
-              Add rice and stir very gently to distribute. Top with artichokes and
-              peppers, and cook without stirring, until most of the liquid is absorbed,
-              15 to 18 minutes. Reduce heat to medium-low, add reserved shrimp and
-              mussels, tucking them down into the rice, and cook again without
-              stirring, until mussels have opened and rice is just tender, 5 to 7
-              minutes more. (Discard any mussels that don&apos;t open.)
-            </Typography>
-            <Typography>
-              Set aside off of the heat to let rest for 10 minutes, and then serve.
-            </Typography>
-          </CardContent>
-        </Collapse>
       </Card>
     );
   }
